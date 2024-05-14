23980fd7d39b00b605df730e348f3a5b0a56d3a3
Gideon Ipiranga Alves <gideon.fame.94@gmail.com>
2024-05-14 00:01:23 -0300

    docker compose, dockerfile, module database

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,52 @@
+import { Body,
+         Controller,
+	     Delete,
+	     Get,
+	     HttpCode,
+	     HttpStatus,
+		 Param,	
+	     Post,
+		 Put,
+		 } from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { createCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
+@Controller('courses')
+export class CoursesController {
+
+	constructor(private readonly courseService: CoursesService){
+		
+	}
+    @Get()
+	findAll(){
+		return this.courseService.findAll()
+	}
+
+
+	@Get(':id')
+	findOne(@Param('id') id: number) {
+		return this.courseService.findOne(id)
+	}
+
+	//@HttpCode(204)
+    @Post()
+	create(@Body() createCourseDTO: createCourseDTO) {
+		return this.courseService.create(createCourseDTO) 
+		
+	}
+    
+	@Put(':id')
+	update(@Param('id') id: number, @Body() UpdateCourseDTO: UpdateCourseDTO){
+		return  this.courseService.update( id, UpdateCourseDTO);
+	}
+     
+
+
+	@HttpCode(HttpStatus.NO_CONTENT)
+    @Delete(':id')
+	remove(@Param('id') id: number, ){
+		return  this.courseService.remove(id);
+
+	}
+}
+
---
+++ src/courses/dto/update-course.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from "@nestjs/mapped-types";
+import { createCourseDTO } from "./create-course.dto";
+
+export class UpdateCourseDTO extends PartialType(createCourseDTO){
+
+}
